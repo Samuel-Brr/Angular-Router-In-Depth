51c73ce0563ec47d501f987dd598e71ff454d341
Samuel-Brr <samuel.barre972@gmail.com>
2022-03-15 00:31:47 +0100

    Router Path Variables - Variable Target Routes with the Router Link Directive

--- src/app/courses/courses-routing.module.ts
+++ src/app/courses/courses-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CourseComponent } from './course/course.component';
 import { HomeComponent } from './home/home.component';
 
 
@@ -7,6 +8,10 @@ const routes: Routes = [
   {
     path:"",
     component: HomeComponent
+  },
+  {
+    path:":courseUrl",
+    component: CourseComponent
   }
 ];
 
