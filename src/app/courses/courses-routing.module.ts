a4262b18ab68520628c50a55215d867d2d234558
Samuel-Brr <samuel.barre972@gmail.com>
2022-03-15 02:17:04 +0100

    Angular Router Child Routes - Step-by-Step Explanation

--- src/app/courses/courses-routing.module.ts
+++ src/app/courses/courses-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CourseComponent } from './course/course.component';
 import { HomeComponent } from './home/home.component';
+import { LessonDetailComponent } from './lesson/lesson-detail.component';
+import { LessonsListComponent } from './lessons-list/lessons-list.component';
 import { CourseResolver } from './services/course.resolver';
 
 
@@ -13,6 +15,16 @@ const routes: Routes = [
   {
     path:":courseUrl",
     component: CourseComponent,
+    children:[
+      {
+        path:"",
+        component: LessonsListComponent
+      },
+      {
+        path: "lessons/:lessonSeqNo",
+        component: LessonDetailComponent,
+      }
+    ],
     resolve: {
       course: CourseResolver
     }
