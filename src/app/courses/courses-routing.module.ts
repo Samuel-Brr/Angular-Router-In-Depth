5238437aa646a769e7e38cf464c135b7f39b4cc4
Samuel-Brr <samuel.barre972@gmail.com>
2022-03-15 13:16:33 +0100

    CanActivate Router Guards - Authentication Guards

--- src/app/courses/courses-routing.module.ts
+++ src/app/courses/courses-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from '../services/auth.guard';
 import { CourseComponent } from './course/course.component';
 import { HomeComponent } from './home/home.component';
 import { LessonDetailComponent } from './lesson/lesson-detail.component';
@@ -17,6 +18,7 @@ const routes: Routes = [
   {
     path:":courseUrl",
     component: CourseComponent,
+    canActivate:[AuthGuard],
     children:[
       {
         path:"",
@@ -48,6 +50,7 @@ const routes: Routes = [
     CourseResolver,
     LessonsResolver,
     LessonDetailResolver,
+    AuthGuard
   ]
 })
 export class CoursesRoutingModule {
